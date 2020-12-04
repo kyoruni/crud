af7987c7599eebe8137c3d1b19297f64286c10d4
kyoruni <kyoruni@gmail.com>
2020-12-04 20:10:03 +0900

    :wrench: #5 テーブル作成

--- rails-crud/db/migrate/20201204085842_create_types.rb
+++ rails-crud/db/migrate/20201204085842_create_types.rb
@@ -1,7 +1,7 @@
 class CreateTypes < ActiveRecord::Migration[6.0]
   def change
-    create_table :types do |t|
-
+    create_table :types, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin', comment: 'タイプ' do |t|
+      t.string :name, null: false, limit: 5, comment: '名前'
       t.timestamps
     end
   end
